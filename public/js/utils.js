30e65dab3f8fa9a4251af1285ba1e0377b50111f
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-03-03 14:49:28 -0500

    fix scroll bar

--- public/js/utils.js
+++ public/js/utils.js
@@ -5,8 +5,16 @@
             score.classList.add('movie__score--bad');
     })
 })();
+(function() {
+    const body = document.querySelector('body');
+    const banner = document.querySelector('.banner');
+    if(!banner) 
+        body.style.overflow = 'scroll';
+
+})();
 
 const closeButton = document.querySelector('.banner__btn-close');
+
 closeButton.addEventListener('click', () => {
     const body = document.querySelector('body');
     const banner = document.querySelector('.banner');
