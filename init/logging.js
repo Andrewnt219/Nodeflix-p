a6b8e13d0f105e7e0d343d66a9899995460794e4
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-03-11 21:40:38 -0400

    update dashboard

--- init/logging.js
+++ init/logging.js
@@ -2,17 +2,6 @@ const winston = require('winston');
 require('express-async-errors');
 
 module.exports = function() {
-    winston.add(new winston.transports.File({
-        filename: 'log.log',
-        handleExceptions: true,
-        format: winston.format.combine(winston.format.colorize())
-    }));
-    winston.add(new winston.transports.Console({
-        handleExceptions: true,
-        format: winston.format.combine(winston.format.colorize()),
-        level: 'error'
-    }));
-
     // winston catches this
     process.on('unhandledRejection', ex => {
         throw ex;
